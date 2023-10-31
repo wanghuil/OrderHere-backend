cd917fe73dda7e9b28bdb318dd8d4839cc7d7f63
SteveGuoh <127906105+SteveGuoh@users.noreply.github.com>
2023-10-31 19:25:06 +0800

    Oj 64 mock data (#39)
    * feat: insert mockdata to database
    
    * delete ds
    
    * fix: dish images

--- src/main/resources/db/migration/V2__insert_mockdata.sql
+++ src/main/resources/db/migration/V2__insert_mockdata.sql
@@ -45,10 +45,10 @@ VALUES (1, 'Main Dishes');
 
 INSERT INTO dish (restaurant_id, dish_name, description, price, image_url, rating, availability, category_id)
 VALUES
-    (1, 'Pasta Carbonara', 'Classic Italian pasta dish with bacon and eggs', 12.99, 'https://example.com/pasta.jpg', 4.5, true, 1),
-    (1, 'Margherita Pizza', 'Traditional pizza with tomato sauce, mozzarella, and basil', 9.99, 'https://example.com/pizza.jpg', 4.2, true, 1),
-    (1, 'Sushi Roll', 'Assorted sushi rolls with fresh seafood', 14.99, 'https://example.com/sushi.jpg', 4.7, true, 1),
-    (1, 'Teriyaki Chicken', 'Grilled chicken with teriyaki sauce and steamed rice', 10.99, 'https://example.com/chicken.jpg', 4.0, true, 1);
+    (1, 'Pasta Carbonara', 'Classic Italian pasta dish with bacon and eggs', 12.99, 'https://plus.unsplash.com/premium_photo-1671547330493-b07d377085ca?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFzdGF8ZW58MHx8MHx8fDA%3D', 4.5, true, 1),
+    (1, 'Margherita Pizza', 'Traditional pizza with tomato sauce, mozzarella, and basil', 9.99, 'https://images.unsplash.com/photo-1513104890138-7c749659a591?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGl6emF8ZW58MHx8MHx8fDA%3D', 4.2, true, 1),
+    (1, 'Sushi Roll', 'Assorted sushi rolls with fresh seafood', 14.99, 'https://plus.unsplash.com/premium_photo-1668143362936-ce8a84410659?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3VzaGklMjByb2xsfGVufDB8fDB8fHww', 4.7, true, 1),
+    (1, 'Teriyaki Chicken', 'Grilled chicken with teriyaki sauce and steamed rice', 10.99, 'https://images.unsplash.com/photo-1609183480237-ccbb2d7c5772?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8VGVyaXlha2klMjBDaGlja2VufGVufDB8fDB8fHww', 4.0, true, 1);
 
 INSERT INTO link_ingredient_dish (dish_id, ingredient_id, quantity_value, quantity_unit)
 VALUES
