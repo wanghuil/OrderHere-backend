38bba4ce3c8c52d47df09c51c5464b274193c6a0
SteveGuoh <127906105+SteveGuoh@users.noreply.github.com>
2023-11-28 00:33:06 +1100

    fix: JWT extraction (#61)

--- src/main/java/com/backend/orderhere/config/StaticConfig.java
+++ src/main/java/com/backend/orderhere/config/StaticConfig.java
@@ -1,16 +1,21 @@
 package com.backend.orderhere.config;
 
 public class StaticConfig {
-  public final static String[] ignoreUrl = new String[]{
-      // -- Swagger UI v2
-      "/v3/api-docs/**",
-      "/swagger-ui/**",
-      // -- API
-      "/v1/public/**",
-  };
+    public final static String[] ignoreUrl = new String[]{
+            // -- Swagger UI v2
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            // -- API
+            "/v1/public/dish/**",
+            "/v1/public/restaurants/**",
+            "/v1/public/users/**",
+            "/v1/public/ingredients/**",
+            "/v1/public/category/**",
 
-  // JWT
+    };
+
+    // JWT
 //    public final static String JwtSecretKey = "JwtSecretKey";
-  public final static String JwtPrefix = "Bearer ";
-  public final static String JwtSecretKey = "Xh8wJZcPZfa7t2WdKr4zr9TnRfWP8xYhV1a8dYkHfTw=";
+    public final static String JwtPrefix = "Bearer ";
+    public final static String JwtSecretKey = "Xh8wJZcPZfa7t2WdKr4zr9TnRfWP8xYhV1a8dYkHfTw=";
 }
